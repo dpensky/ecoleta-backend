f545789b611b29c126829dca176fb959e5efd08d
Darcio Pensky <darcio@studiobluechip.com>
2020-06-02 13:05:17 -0300

    add new routes

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 backend do aplicativo ecoleta desenvolvido no tutorial da Rocketseat
 
 Backend em javascript do aplicativo desenvolvido durante o #NLW da Rocketset em Maio de 2020
+npm install
--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,41 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json())
+
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Daniel'
+];
+
 /*
 * http://localhost:3333/users
 */
 app.get('/users', (request, response) => {
-    console.log('Listagem de Usuarios');
+    // console.log('Listagem de Usuarios');
     // response.send('Hello World!');
-    response.json([
-        'Diego',
-        'Cleiton',
-        'Robson',
-        'Daniel'
-    ]);
+    const search = String(request.query.search);
+    // console.log(search)
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+    return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const user = users[id];
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+    console.log(data)
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+    return response.json(user);
 });
 
 app.listen(3333);
